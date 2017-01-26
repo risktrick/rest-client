d8e44259ddf04f1e04818b4f90db7e3af88fc301
Uzerro <user@mail.com>
2017-01-26 08:23:31 +0300

    test refactoring

--- app/src/test/java/com/example/restclient/model/JsonParserUnitTest.java
+++ app/src/test/java/com/example/restclient/model/JsonParserUnitTest.java
@@ -20,6 +20,9 @@ import static org.junit.Assert.*;
 @PrepareForTest({Log.class})
 public class JsonParserUnitTest {
 
+    JsonParser parser;
+    ILogger logger;
+
     @Before
     public void setup() {
         PowerMockito.mockStatic(Log.class, new Answer() {
@@ -29,37 +32,43 @@ public class JsonParserUnitTest {
                 return null;
             }
         });
+
+        parser = new JsonParser();
+        logger = new LoggerConsole();
+    }
+
+    @Test
+    public void parseBashJson() throws Exception {
+
     }
 
     @Test
     public void parseOneTestModel() {
-        JsonParser parser = new JsonParser();
         String response = "{name:\"John\", age:\"32\"}";
-        ILogger logger = new LoggerConsole();
         parser.parseOneTestModel(response, logger);
     }
 
     @Test
-    public void parseArrayTestModelNewMethod() {
-        JsonParser parser = new JsonParser();
+    public void parseArrayTestModel() throws Exception {
         String response = "[{name:\"John\", age:\"32\"}, {name:\"Mike\", age:\"45\"}]";
-        ILogger logger = new LoggerConsole();
+        parser.parseArrayTestModelNewMethod(response, logger);
+    }
+
+    @Test
+    public void parseArrayTestModelNewMethod() {
+        String response = "[{name:\"John\", age:\"32\"}, {name:\"Mike\", age:\"45\"}]";
         parser.parseArrayTestModelNewMethod(response, logger);
     }
 
     @Test
     public void parseArrayOfArrayTestModel() {
-        JsonParser parser = new JsonParser();
         String response = "[[{name:\"John\", age:\"32\"}, {name:\"Mike\", age:\"45\"}], [{name:\"Marian\", age:\"18\"}]]";
-        ILogger logger = new LoggerConsole();
         parser.parseArrayOfArrayTestModel(response, logger);
     }
 
     @Test
     public void parseSources() {
-        JsonParser parser = new JsonParser();
         String response = "[[{\"site\":\"bash.im\",\"name\":\"bash\",\"url\":\"http://bash.im\",\"parsel\":\".text\",\"encoding\":\"windows-1251\",\"linkpar\":\"/quote/\",\"desc\":\"Цитатник Рунета\"},{\"site\":\"bash.im\",\"name\":\"abyss\",\"url\":\"http://bash.im/abyss\",\"parsel\":\".text\",\"encoding\":\"windows-1251\",\"linkpar\":\"none\",\"desc\":\"Цитатник Рунета - Бездна\"}],[{\"site\":\"ithappens.me\",\"name\":\"ithappens\",\"url\":\"http://ithappens.me\",\"parsel\":\".text\",\"encoding\":\"UTF-8\",\"linkpar\":\"/story/\",\"desc\":\"IT Happens\"}],[{\"site\":\"zadolba.li\",\"name\":\"zadolbali\",\"url\":\"http://zadolba.li\",\"parsel\":\".text\",\"encoding\":\"UTF-8\",\"linkpar\":\"/story/\",\"desc\":\"Задолба!ли\"}],[{\"site\":\"anekdot.ru\",\"name\":\"new anekdot\",\"url\":\"http://www.anekdot.ru/last/j.html\",\"parsel\":\".text\",\"encoding\":\"windows-1251\",\"linkpar\":\"/id/\",\"desc\":\"Свежие анекдоты\"},{\"site\":\"anekdot.ru\",\"name\":\"new story\",\"url\":\"http://www.anekdot.ru/last/o.html\",\"parsel\":\".text\",\"encoding\":\"windows-1251\",\"linkpar\":\"/id/\",\"desc\":\"Новые истории\"},{\"site\":\"anekdot.ru\",\"name\":\"new aforizm\",\"url\":\"http://www.anekdot.ru/last/a.html\",\"parsel\":\".text\",\"encoding\":\"windows-1251\",\"linkpar\":\"/id/\",\"desc\":\"Новые афоризмы и фразы\"},{\"site\":\"anekdot.ru\",\"name\":\"new stihi\",\"url\":\"http://www.anekdot.ru/last/c.html\",\"parsel\":\".text\",\"encoding\":\"windows-1251\",\"linkpar\":\"/id/\",\"desc\":\"Стишки\"}],[{\"site\":\"ideer.ru\",\"name\":\"ideer\",\"url\":\"http://ideer.ru\",\"parsel\":\".shortContent\",\"encoding\":\"UTF-8\",\"linkpar\":\"/\",\"desc\":\"Подслушано\"}],[{\"site\":\"det.org.ru\",\"name\":\"Deti\",\"url\":\"http://det.org.ru\",\"parsel\":\".history\",\"encoding\":\"windows-1251\",\"linkpar\":\"/?quote=\",\"desc\":\"Говорят дети\"}],[{\"site\":\"xkcdb.com\",\"name\":\"XKCDB\",\"url\":\"http://www.xkcdb.com\",\"parsel\":\"span.quote\",\"encoding\":\"UTF-8\",\"linkpar\":\"/\",\"desc\":\"XKCDB\"}]]\n";
-        ILogger logger = new LoggerConsole();
         parser.parseSources(response, logger);
     }
 }
